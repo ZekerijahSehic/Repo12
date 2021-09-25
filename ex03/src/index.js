55ae676a9e4ad3339ead7856876341d03ef2aa75
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-25 09:11:28 +0200

    Range of Nums | Recursion

---
+++ ex03/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this list
+function rangeOfNumbers(startN, endN) {
+    var nums = [];
+    if (startN < endN) {
+        var nums = rangeOfNumbers(startN, endN - 1);
+        (nums.push(endN));
+        return nums;  
+    } else if ( startN == endN) {
+        return [startN];
+    } else {
+        return "The starting number will always be less than or equal to the ending number";
+    }
+  }
+// Only change code above this list
+
+console.log(rangeOfNumbers(1,7))
+console.log(rangeOfNumbers(3,10))
+console.log(rangeOfNumbers(5,5))
+console.log(rangeOfNumbers(7,5))
+module.exports = rangeOfNumbers;
