fdd8bc4e8735cb6eb60514393f0ac7db53e43c96
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-25 09:29:38 +0200

    Range of Nums | Recursion

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -13,8 +13,8 @@ function rangeOfNumbers(startN, endN) {
   }
 // Only change code above this list
 
-console.log(rangeOfNumbers(1,7))
-console.log(rangeOfNumbers(3,10))
-console.log(rangeOfNumbers(5,5))
-console.log(rangeOfNumbers(7,5))
+console.log(rangeOfNumbers(1,7));
+console.log(rangeOfNumbers(3,10));
+console.log(rangeOfNumbers(5,5));
+console.log(rangeOfNumbers(7,5));
 module.exports = rangeOfNumbers;
